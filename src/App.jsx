7bf64992d361b6cda7d8b3d24a235945f68958b9
Vals098 <valeria.farinosi@gmail.com>
2026-05-03 17:09:34 +0200

    import: profile and settings page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css"
 import NetflixNavbar from "./components/NetflixNavbar"
 import NetflixFooter from "./components/NetflixFooter"
 import NetflixCarousel from "./components/NetflixCarousel"
+import NetflixProfile from "./components/NetflixProfile"
+import NetflixSettings from "./components/NetflixSettings"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <NetflixCarousel query="harry potter" title="Harry Potter Saga" />
       <NetflixCarousel query="matrix" title="Matrix Saga" />
       <NetflixFooter></NetflixFooter>
+      <NetflixProfile></NetflixProfile>
+      <NetflixSettings></NetflixSettings>
     </>
   )
 }
